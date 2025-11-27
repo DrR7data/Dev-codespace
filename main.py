ece022e0c2b84e4d8bb54484a18d33bca2c4f535
DrR7data <robertorm29@gmail.com>
2025-11-27 04:15:17 +0000

    Edit main

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-def hi():
-    return "Hi"
+#!/usr/bin/env python
+import click
+import glob
+
+@click.command()
+@click.option(
+    "--path",
+    prompt="Path to search for cvs files",
+    help="This is the path to search for files; /tmp",
+)
+@click.option(
+    "--ftype", 
+    prompt="Pass in the type of file",
+    help="Pass in the file type: i.e csv",
+)
+def search(path, ftype):
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style(f"Found Matches:", fg="red"))
+    for result in results:
+        click.echo(click.style(f"{result}", bg="blue", fg="white"))
+
+
+if __name__ == "__main__":
+    search()
